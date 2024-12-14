5bf6ac18b52f46cff00c1601da75afe832537781
d-pamneja <dpamneja@gmail.com>
2024-12-14 23:53:05 +0530

    Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       try {
           const response = await fetch('https://cors-anywhere.herokuapp.com/corsdemo', {
               method: 'GET',
+              mode: 'no-cors'
           });
           if (response.ok) {
               console.log('CORS-Anywhere demo server activated successfully.');
