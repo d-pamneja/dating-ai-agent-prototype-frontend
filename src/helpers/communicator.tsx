3935f046d1a9bdb455c8faaa6ac749fab297fd0a
d-pamneja <dpamneja@gmail.com>
2024-12-15 00:04:54 +0530

    Deployment

--- src/helpers/communicator.tsx
+++ src/helpers/communicator.tsx
@@ -1,21 +1,21 @@
-const aiMindURL = "http://13.233.115.212"
+const proxyURL = "https://proxy-backend.vercel.app/api";
+const aiMindURL = "http://13.233.115.212";
 
-export const getQuestion = async (conversation : any) => {
-    const res = await fetch(`https://cors-anywhere.herokuapp.com/${aiMindURL}/aiAgent/getQuestion`, {
+export const getQuestion = async (conversation: any) => {
+    const res = await fetch(`${proxyURL}?url=${encodeURIComponent(`${aiMindURL}/aiAgent/getQuestion`)}`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json', 
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ 
-            conversation : conversation
+        body: JSON.stringify({
+            conversation: conversation,
         }),
-    })
-    
+    });
 
     if (res.status !== 200) {
         throw new Error("Unable to fetch answer to user query");
     }
 
-    const data = await res.json(); 
+    const data = await res.json();
     return data;
 };
